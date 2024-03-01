6127706a77e4975ed36b6f8ee00e97996d31b854
Nicholas Kernan <nicholas.kernan@gmail.com>
2024-03-01 00:57:08 -0500

    script adding data from latest month

--- github_script.sh
+++ github_script.sh
@@ -1,8 +1,12 @@
 month=$1 
 
 cd netlify-site
+git add "data/months.json"
 git add "data/$month"
 git add "images/$month"
-string="script - adding data from $month"
-git commit -m $string
+
+git commit -m "script adding data from latest month"
 git push
+
+#./netlify-site/github_script.sh January_2024
+#python3 github_python.py
